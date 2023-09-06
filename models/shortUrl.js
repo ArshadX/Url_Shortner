04092a13a632295c4f4c70fa0500d2c94855ab39
ArshadX <mas12542@outlook.com>
2023-09-06 23:46:12 +0530

    emailList

--- models/shortUrl.js
+++ models/shortUrl.js
@@ -1,19 +1,19 @@
 const mongoose = require("mongoose");
-const shortid = require("shortid");
+// const shortid = require("shortid");
 const schema = new mongoose.Schema({
-  full: {
+  emailList: {
     type: String,
     required: true,
   },
-  short: {
-    type: String,
-    required: true,
-    default: shortid.generate,
-  },
-  clicks: {
-    type: Number,
-    required: true,
-    default: 0,
-  },
+  // short: {
+  //   type: String,
+  //   required: true,
+  //   default: shortid.generate,
+  // },
+  // clicks: {
+  //   type: Number,
+  //   required: true,
+  //   default: 0,
+  // },
 });
-module.exports = mongoose.model("ShortUrl", schema);
+module.exports = mongoose.model("emailList", schema);
--- server.js
+++ server.js
@@ -1,34 +1,38 @@
 const express = require("express");
 const app = express();
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 
-const ShortUrl = require("./models/shortUrl");
+// const emailList = require("./models/shortUrl");
 
-mongoose.connect("mongodb://127.0.0.1:27017/test").then(
-  () => {
-    console.log("connected");
-  },
-  (err) => {
-    console.log(err);
-  }
-);
+// mongoose
+//   .connect("mongodb://127.0.0.1:27017/test")
+//   .then(() => {
+//     console.log("connected");
+//   })
+//   .catch((err) => console.log(err));
+const emailList = [];
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: false }));
 app.get("/", async (req, res) => {
-  const shortUrl = await ShortUrl.find();
-  res.render("index", { shortUrl: shortUrl });
+  // const emailList = await emailList.find();
+  res.send("server online...");
 });
 
-app.post("/shortUrls", async (req, res) => {
-  console.log(req.body.fullUrl);
-  await ShortUrl.create({ full: req.body.fullUrl });
-  res.redirect("/");
+app.post("/email=:email", async (req, res) => {
+  // console.log(req.body.fullUrl);
+  // await ShortUrl.create({ full: req.body.fullUrl });
+  if (typeof req.params.email === "string") {
+    emailList.push(req.params.email);
+    res.status(200).send("succesfull");
+  } else {
+    res.status(404).send("must be string");
+  }
 });
-app.get("/:shortUrl", async (req, res) => {
-  const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl });
-  if (shortUrl == null) return res.sendStatus(404);
-  shortUrl.clicks++;
-  shortUrl.save();
-  res.redirect(shortUrl.full);
+app.get("/emailList", async (req, res) => {
+  // const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl });
+  // if (shortUrl == null) return res.sendStatus(404);
+  // shortUrl.clicks++;
+  // shortUrl.save();
+  emailList.length === 0 ? res.status(200).send("List is empty") : res.status(200).send(emailList);
 });
 app.listen(process.env.WEB_CONCURRENCY || 3000);
